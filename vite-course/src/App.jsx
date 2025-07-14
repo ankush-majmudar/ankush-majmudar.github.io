4936f48290e2c2cee31b23e551b910b800ec9255
ankush <amajmudar@student.ethz.ch>
2025-07-14 23:26:01 +0200

    added rf logo next to the vite and react logos and learnt about static assets (like images). placing them under assets in src makes them optimize in the build by hashing, inlining if <4kb. files which you dont need to optimize can stay in public and will go as is

--- vite-course/src/App.jsx
+++ vite-course/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import rfLogo from './assets/rflogo.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
@@ -17,6 +18,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://rogerfederer.com" target="_blank">
+          <img src={rfLogo} className="logo react" alt="RF logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
